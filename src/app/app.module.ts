1fa9fc70e319103ffa632a23f766d1a46984c004
Anna <anka1-ania@windowslive.com>
2020-05-22 19:15:11 +0200

    lslj

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,12 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-<<<<<<< HEAD
 import {MatButtonModule} from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent} from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { from } from 'rxjs';
-=======
-import { MatButtonModule } from '@angular/material/button';
-
-
-import { AppComponent } from './app.component';
-import { HeaderComponent} from './header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
->>>>>>> anna
 
 
 @NgModule({
@@ -34,13 +25,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatButtonModule,
     RouterModule.forRoot([
 //Vielleicht stehen die Komponenten hier für den outlet
-<<<<<<< HEAD
 { path: 'login', component: LoginComponent},
     ]),
-=======
-    ]),
-    BrowserAnimationsModule
->>>>>>> anna
   ],
   providers: [
 
