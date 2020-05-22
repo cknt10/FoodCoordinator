54b7f4362fd137e710abeabbee0cdad7a156c5e4
unknown <FlashPippin@gmx.net>
2020-05-22 17:39:46 +0200

    irgendwas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,30 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
-
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent} from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { from } from 'rxjs';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    LoginComponent,
 
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     HttpClientModule,
+    MatButtonModule,
     RouterModule.forRoot([
 //Vielleicht stehen die Komponenten hier für den outlet
-    ])
+{ path: 'login', component: LoginComponent},
+    ]),
   ],
   providers: [
 
