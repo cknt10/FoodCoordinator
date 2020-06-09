6da780fe334de196bea9ade47fa578157bfe7f43
Natalia Pfening <natalia.pfening@web.de>
2020-06-09 18:02:47 +0200

    function fix

--- api/classes/reguser.php
+++ api/classes/reguser.php
@@ -721,8 +721,8 @@ class RegUser{
           "birthday" => $this->_birthday,
           "postcode" => $this->_postcode,
           "location" => $this->_location,
-          "orders" => empty($this->_orders) ? null : $this->orders,
-          "recipes" => empty($this->_recipes) ? null : $this->recipes,
+          "orders" => empty($this->_orders) ? null : $this->_orders,
+          "recipes" => empty($this->_recipes) ? null : $this->_recipes,
         );
 
     }
