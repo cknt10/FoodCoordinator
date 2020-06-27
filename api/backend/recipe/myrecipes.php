27d2b2f2cf8f0c7dafb974739ad5845ff2445841
sergej-thm <sergej.allerdings@iem.thm.de>
2020-06-27 20:10:18 +0200

    fix myrecipes

--- api/backend/recipe/myrecipes.php
+++ api/backend/recipe/myrecipes.php
@@ -36,57 +36,68 @@ $result['recipes'] = array();
 
 $recipies = $recipe->fetchAllRecipe($userId);
 
-if($recipies->rowCount() > 0){
-    // retrieve our table contents
-    // fetch() is faster than fetchAll()
-    // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
-    while ($_row = $recipies->fetch(PDO::FETCH_ASSOC)){
-        // extract row
-        // this will make $row['name'] to
-        // just $name only
-        extract($_row);
-
-        $recipe->setId($R_ID);
-        $recipe->setTitle($Title);
-        $recipe->setPicture($Picture);
-        $recipe->setServings($Servings);
-        $recipe->setDescription($R_Descr);
-        $recipe->setInstruction($Instruction);
-        $recipe->setCreationDate($CreationDate);
-        $recipe->setDuration($Duration);
-        $recipe->setDifficulty($Difficulty);
-        $recipe->setCertified($Certified);
-        $recipe->setLastChange($LastChange);
-        $recipe->setCreatedUser($U_ID);
-
-        $ratings = $rating->fetchRatings($R_ID, $U_ID);
-
-        if($ratings != null && $R_ID === $ratings[0]['recipeId']){
-            $recipe->setRatings($ratings);
-            $recipe->calculateRating();
-        }else{
-            $recipe->setRatings(array());
+if($userId != ""){
+    if($recipies->rowCount() > 0){
+        // retrieve our table contents
+        // fetch() is faster than fetchAll()
+        // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
+        while ($_row = $recipies->fetch(PDO::FETCH_ASSOC)){
+            // extract row
+            // this will make $row['name'] to
+            // just $name only
+            extract($_row);
+    
+            $recipe->setId($R_ID);
+            $recipe->setTitle($Title);
+            $recipe->setPicture($Picture);
+            $recipe->setServings($Servings);
+            $recipe->setDescription($R_Descr);
+            $recipe->setInstruction($Instruction);
+            $recipe->setCreationDate($CreationDate);
+            $recipe->setDuration($Duration);
+            $recipe->setDifficulty($Difficulty);
+            $recipe->setCertified($Certified);
+            $recipe->setLastChange($LastChange);
+            $recipe->setCreatedUser($U_ID);
+    
+            $ratings = $rating->fetchRatings($R_ID, $U_ID);
+    
+            if($ratings != null && $R_ID === $ratings[0]['recipeId']){
+                $recipe->setRatings($ratings);
+                $recipe->calculateRating();
+            }else{
+                $recipe->setRatings(array());
+            }
+            //$recipe->setRatings($ratings);
+    
+            array_push($result['recipes'], $recipe->getObjectAsArray());
+    
         }
-        //$recipe->setRatings($ratings);
-
-        array_push($result['recipes'], $recipe->getObjectAsArray());
-
-    }
-
-    if(count($result['recipes']) > 0){
-        // set response code - 200
-        http_response_code(200);
-
-        $result["message"] = "";
+    
+        if(count($result['recipes']) > 0){
+            // set response code - 200
+            http_response_code(200);
+    
+            $result["message"] = "";
+            echo json_encode($result);
+        }
+    }else{
+        // set response code - 404
+        http_response_code(404);
+    
+        $result["message"] = "No recipe found";
         echo json_encode($result);
     }
-}else{
-    // set response code - 404
-    http_response_code(404);
 
-    $result["message"] = "No recipe found";
-    echo json_encode($result);
+}else{
+        // set response code - 403
+        http_response_code(403);
+
+        $result["message"] = "No user id";
+        echo json_encode($result);
 }
+
+
 
 
 
