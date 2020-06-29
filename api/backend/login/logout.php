bbb44b28c995e8c6ed12b8ae1d5008b03040d91b
sergej-thm <sergej.allerdings@iem.thm.de>
2020-06-29 09:46:18 +0200

    logged out

--- api/backend/login/logout.php
+++ api/backend/login/logout.php
@@ -2,5 +2,10 @@
 session_start();
 session_destroy();
  
-echo "Logout erfolgreich";
+// set response code - 200
+http_response_code(200);
+
+// tell the user no result found
+$result["message"] = "User loggedout";
+echo json_encode($result);
 ?>
