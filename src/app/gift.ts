c313edf09de47b25b0b4d969b16c1a0fda510510
Anna <anka1-ania@windowslive.com>
2020-06-29 19:51:37 +0200

    change from interface to class

--- src/app/gift.ts
+++ src/app/gift.ts
@@ -1,8 +1,48 @@
-export interface Gift{
-    id: number;
-    reedemDay: Date;
-    startDay: Date;
-    EndDay: Date;
-    isRedeem: boolean;
-    idUser: number;
+export class Gift{
+    private id: number;
+    private reedemDay: Date;
+    private startDay: Date;
+    private endDay: Date;
+    private isRedeem: boolean;
+
+    getId(): number{
+        return this.id;
+    }
+
+    getReedemDay(): Date{
+        return this.reedemDay;
+    }
+
+    getStartDay(): Date{
+        return this.startDay;
+    }
+
+    getEndDay(): Date{
+        return this.endDay;
+    }
+
+    getIsRedeem(): boolean{
+        return this.isRedeem;
+    }
+
+    setId(id: number){
+        this.id = id;
+    }
+
+
+    setReedemDay(reedemDay: Date){
+        this.reedemDay = reedemDay;
+    }
+
+    setStartDay(startDay: Date) {
+        this.startDay = startDay;
+    }
+
+    setEndDay(endDay: Date){
+        this.endDay = endDay;
+    }
+
+    setIsRedeem(isRedeem: boolean){
+        this.isRedeem = isRedeem;
+    }
 }
