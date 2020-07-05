aea0c63a687bf37b8d27360d62cec050b3104f40
Lord_Gulasch <Christian.Knoth@iem.thm.de>
2020-07-05 08:23:25 +0200

    deleted unnecessary allocation

--- src/app/content/create-recipe/create-recipe.component.ts
+++ src/app/content/create-recipe/create-recipe.component.ts
@@ -100,8 +100,7 @@ export class CreateRecipeComponent implements OnInit {
     let text = reader;
     reader.onload = (event:any) =>{
       console.log(text.result)
-      this.imageUrl = event.target.result;
-      this.picture = <string>text.result;
+      this.imageUrl = <string>text.result;
     };
   }
 
