488af478648df33b63332ea432bc757ef1f26908
sergej-thm <sergej.allerdings@iem.thm.de>
2020-07-06 11:30:37 +0200

    testing recipeset something is wrong

--- api/backend/recipe/recipeset.php
+++ api/backend/recipe/recipeset.php
@@ -70,88 +70,88 @@ $dummyarray['ingredients'] = $ingredinetsFrontend === "" ? aray() : $ingredinets
 $dummyarray['keywords'] = empty($keywordsFrontend) ? array() : $keywordsFrontend;
 
 
-//Create return 
-$recipeArray = array();
-$recipeArray["message"] = "";
+// //Create return 
+// $recipeArray = array();
+// $recipeArray["message"] = "";
 
-//Check recipe exists
+// //Check recipe exists
 
-$checkRecipe = $recipe->fetchRecipe($dummyarray['creationDate'], $dummyarray['userId']);
-if($checkRecipe == -1){
-    //Create Recipe
-    $checkRecipeCreated = $recipe->createRecipe($dummyarray['title'], 
-                                                $dummyarray['picture'], 
-                                                $dummyarray['servings'], 
-                                                $dummyarray['description'], 
-                                                $dummyarray['instruction'],
-                                                $dummyarray['creationDate'],
-                                                $dummyarray['duration'],
-                                                $dummyarray['difficulty'],
-                                                $dummyarray['certified'],
-                                                $dummyarray['lastChange'],
-                                                $dummyarray['userId']);
+// $checkRecipe = $recipe->fetchRecipe($dummyarray['creationDate'], $dummyarray['userId']);
+// if($checkRecipe == -1){
+//     //Create Recipe
+//     $checkRecipeCreated = $recipe->createRecipe($dummyarray['title'], 
+//                                                 $dummyarray['picture'], 
+//                                                 $dummyarray['servings'], 
+//                                                 $dummyarray['description'], 
+//                                                 $dummyarray['instruction'],
+//                                                 $dummyarray['creationDate'],
+//                                                 $dummyarray['duration'],
+//                                                 $dummyarray['difficulty'],
+//                                                 $dummyarray['certified'],
+//                                                 $dummyarray['lastChange'],
+//                                                 $dummyarray['userId']);
 
 
-    sleep(3);
-    if(strcmp($checkRecipeCreated, "200")){
-        //get created recipe id
-        $recipeId = $recipe->fetchRecipe($dummyarray['creationDate'], $dummyarray['userId']);
+//     sleep(3);
+//     if(strcmp($checkRecipeCreated, "200")){
+//         //get created recipe id
+//         $recipeId = $recipe->fetchRecipe($dummyarray['creationDate'], $dummyarray['userId']);
 
-        //Ingredient
+//         //Ingredient
 
-        //Insert recipe id to ingredients
-        for($_i = 0; $_i < count($dummyarray['ingredients']); $_i++){
-            $dummyarray['ingredients'][$_i]->r_id= strval($recipeId);
-        }
+//         //Insert recipe id to ingredients
+//         for($_i = 0; $_i < count($dummyarray['ingredients']); $_i++){
+//             $dummyarray['ingredients'][$_i]->r_id= strval($recipeId);
+//         }
 
-        $checkIngredient = $ingredient->createIngredients($dummyarray['ingredients']);
-        if($checkIngredient === "200"){
-            //Keywords
-            $checkKeywords = $recipe->createKeywords($recipeId, $dummyarray['keywords']);
-            if($checkKeywords === "200"){
-                // set response code - 500
-                http_response_code(200);
+//         $checkIngredient = $ingredient->createIngredients($dummyarray['ingredients']);
+//         if($checkIngredient === "200"){
+//             //Keywords
+//             $checkKeywords = $recipe->createKeywords($recipeId, $dummyarray['keywords']);
+//             if($checkKeywords === "200"){
+//                 // set response code - 200
+//                 http_response_code(200);
 
-                $recipeArray["message"] = "Recipe created";
-                echo json_encode($recipeArray);
-            }else{
-                // set response code - 500
-                http_response_code(500);
+//                 $recipeArray["message"] = "Recipe created";
+//                 echo json_encode($recipeArray);
+//             }else{
+//                 // set response code - 500
+//                 http_response_code(500);
                 
-                $recipeArray["message"] = "Cant create keywords";
-                echo json_encode($recipeArray);
-            }
-        }else{
-            // set response code - 500
-            http_response_code(500);
+//                 $recipeArray["message"] = "Cant create keywords";
+//                 echo json_encode($recipeArray);
+//             }
+//         }else{
+//             // set response code - 500
+//             http_response_code(500);
 
-            $recipeArray["message"] = "Cant create ingredient";
-            echo json_encode($recipeArray);
-        }
-    }
-    else{
-        // set response code - 500
-        http_response_code(500);
+//             $recipeArray["message"] = "Cant create ingredient";
+//             echo json_encode($recipeArray);
+//         }
+//     }
+//     else{
+//         // set response code - 500
+//         http_response_code(500);
         
-        $recipeArray["message"] = "Cant create recipe";
-        echo json_encode($recipeArray);
-    }
-}else{
-   // set response code - 404
-   http_response_code(404);
-//    $recipeArray["data"] = $dummyarray;
-//    $recipeArray["keywords"] = $keywordsFrontend;
-//    $recipeArray["ingredients"] = $ingredinetsFrontend;
-   $recipeArray["message"] = "Recipe allready exits";
-   echo json_encode($recipeArray);
-}
+//         $recipeArray["message"] = "Cant create recipe";
+//         echo json_encode($recipeArray);
+//     }
+// }else{
+//    // set response code - 404
+//    http_response_code(404);
+// //    $recipeArray["data"] = $dummyarray;
+// //    $recipeArray["keywords"] = $keywordsFrontend;
+// //    $recipeArray["ingredients"] = $ingredinetsFrontend;
+//    $recipeArray["message"] = "Recipe allready exits";
+//    echo json_encode($recipeArray);
+// }
 
-// $recipeArray["ingredient"] = $dummyarray['ingredients'];
-// $recipeArray["keywords"] = $keywordsFrontend;
-// $recipeArray["ingredients"] = $ingredinetsFrontend;
-// $recipeArray["postdata"] =$postdata;
-// $recipeArray["data"] = $dummyarray;
+$recipeArray["ingredient"] = $dummyarray['ingredients'];
+$recipeArray["keywords"] = $keywordsFrontend;
+$recipeArray["ingredients"] = $ingredinetsFrontend;
+$recipeArray["postdata"] =$postdata;
+$recipeArray["data"] = $dummyarray;
 
-// $recipeArray["message"] = "Recipe already exits";
-// echo json_encode($recipeArray);
+$recipeArray["message"] = "Recipe already exits";
+echo json_encode($recipeArray);
 ?>
