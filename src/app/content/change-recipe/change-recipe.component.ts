ca24b7e30945957a4790db960969c55306eae709
Natalia Pfening <natalia.pfening@web.de>
2020-07-07 23:03:34 +0200

    basis change recipe, redirect from login to content removed
    Co-authored-by: DSTN92 <dustin.bastke@online.de>
    Co-authored-by: cknt10 <FlashPippin@gmx.net>

--- src/app/content/change-recipe/change-recipe.component.ts
+++ src/app/content/change-recipe/change-recipe.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Ingredient} from 'src/app/Ingredient';
+import { Ingredient} from 'src/app/ingredient';
 import { Recipe } from 'src/app/recipe';
 import { ActivatedRoute } from '@angular/router';
 import { AuthenticationService } from 'src/app/authentication.service';
 import { RecipeAdministrationReqService } from 'src/app/recipe-administration-req.service';
+import { SearchReqService } from 'src/app/search-req.service';
+import { FormControl, Validators } from '@angular/forms';
+import { SearchParameter } from 'src/app/searchParameter';
 
 @Component({
   selector: 'app-change-recipe',
@@ -16,56 +19,78 @@ export class ChangeRecipeComponent implements OnInit {
   recipe: Recipe;
   title: String;
   difficulty: String;
+  difficultyOptions: String[] = [];
   shortDescription: String;
-  keyword: String;
+  keyword: string;
   keywords: string[] = [];
-  amount: Number;
-  unit: Number;
-  ingredient: String;
+  options: String[] = [];
+  amount: number;
+  unit: string;
+  units: String[] = [];
+  ingredient: string;
   ingredients: Ingredient[] = [];
-  duration: Number;
-  servings: String;
-  description: String;
-
+  description: string;
+  duration: any;
+  servings: Number;
+  instruction: String;
+  allParamsOfIngredient: Ingredient;
   imageUrl: String;
   fileToUpload: File = null;
   picture: string;
 
+  drop = new FormControl();
+  dro = new FormControl();
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private user: AuthenticationService,
     private recipeAdministrationReqService: RecipeAdministrationReqService,
+    public searchReqService: SearchReqService,
     ) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    this.getRecipe();
 
+    this.difficultyOptions = this.recipeAdministrationReqService.getDifficulty();
+    this.units = this.recipeAdministrationReqService.getUnit();
+    await this.searchReqService.getServerKeywords();
+    await this.searchReqService.getServerIngredients();
   }
 
-  changeRecipe(){
+// Recipe
+  async getRecipe(): Promise<Recipe> {
+    const id = +this.route.snapshot.paramMap.get('id');
 
+    await this.recipeAdministrationReqService
+      .fetchServerRecipeDetails(id)
+      .then((data: Recipe) => {
+        data['recipe'].forEach((value) =>{
+          this.recipe = new Recipe(value);
+
+        })
+      });
+
+      console.log(this.recipe);
+      this.setValues();
+
+    return this.recipe;
   }
 
-  addKeyword(){
-
-  }
-
-  removeKeyword(){
-
-  }
-
-  addIngredient(){
-
-  }
-
-  removeIngredient(){
-
+  // set the Values
+  setValues() {
+    this.title = this.recipe.getTitle();
+    this.difficulty = this.recipe.getDifficulty();
+    this.shortDescription = this.recipe.getDescription();
+    this.keywords = this.recipe.getKeywords();
+    this.ingredients = this.recipe.getIngredients();
+    this.duration = this.recipe.getDuration();
+    this.servings = this.recipe.getServings();
+    this.instruction = this.recipe.getInstruction();
   }
 
   handleFileInput(file: FileList){
     this.fileToUpload= file.item(0);
-
     //Show image preview
     var reader= new FileReader();
     reader.readAsDataURL(this.fileToUpload);
@@ -77,6 +102,70 @@ export class ChangeRecipeComponent implements OnInit {
     };
   }
 
+  addKeyword(){
+    if(!this.keywords.includes(this.keyword)){
+      this.keywords.push(this.keyword);
+    }
+    else{
+      window.alert('Das Schlüsselwort existiert bereits');
+    }
+    this.keyword = '';
+  }
 
+  removeKeyword(i: number){
+    this.keywords.splice(i, 1);
+  }
 
+  addIngredient() {
+    let idIngredient: number;
+    if (this.description != null && this.amount != null && this.unit != null) {
+     idIngredient = this.recipeAdministrationReqService.convertRecipeIngredient(
+      this.description);
+      this.allParamsOfIngredient = new Ingredient(
+        idIngredient,
+        this.description,
+        this.amount,
+        this.unit,
+        null);
+      console.log(idIngredient, this.description, this.amount, this.unit)
+      this.description = null;
+      this.amount = null;
+      this.unit = null;
+
+      this.ingredients.push(this.allParamsOfIngredient);
+      console.log(this.ingredients);
+      this.allParamsOfIngredient = null;
+    }
+    else {
+      window.alert('Bitte füge eine Zutat hinzu!');
+    }
+  }
+
+////piss dich alla
+/*
+  addIngredient(){
+    console.log(this.ingredients);
+    this.ingredient.setDescription(this.description);
+      this.ingredient.setAmount(this.amount);
+      console.log(this.amount);
+      this.ingredient.setUnit(this.unit);
+      this.ingredients.push(this.ingredient);
+    if (!this.ingredients.includes(this.ingredient)){
+
+    }
+    else{
+      window.alert('Diese Zutat existiert bereits in diesem Rezept');
+    }
+    this.amount = null;
+    this.ingredient = null;
+    console.log(this.ingredients);
+  }
+*/
+  removeIngredient(i: number){
+    this.ingredients.splice(i, 1);
+  }
+
+  changeRecipe(){
+
+  }
 }
