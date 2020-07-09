63d2876a2b0f4470b547be51d0cc3470157237cb
Anna <anka1-ania@windowslive.com>
2020-07-09 19:55:42 +0200

    fix usermanagement

--- src/app/content/user-management/user-management.component.ts
+++ src/app/content/user-management/user-management.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-user-management',
   templateUrl: './user-management.component.html',
-  styleUrls: ['./user-management.component.css']
+  styleUrls: ['./user-management.component.css'],
 })
 export class UserManagementComponent implements OnInit {
   user: User;
@@ -22,50 +22,51 @@ export class UserManagementComponent implements OnInit {
   birthday: Date;
   username: String;
   email: String;
-  emailConfirm: String;// finde ich nicht nötig
+  emailConfirm: String; // finde ich nicht nötig
   password: string; // sollte, da man es ohnehin nicht shene kann und nicht weiß was drin ist, leer lassen
-  passwordConfirm: String;//Kontrolle der beiden n icht vergessen :)
+  passwordConfirm: String; //Kontrolle der beiden n icht vergessen :)
   picture: String;
 
   fileToUpload: File = null;
-  imageUrl: String = "";
-
+  imageUrl: String = '';
 
   constructor(
     private router: Router,
     private authenticationService: AuthenticationService,
     private userManagement: UserManagementServiceReqService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.getUser();
     this.setValue();
   }
 
-  async getUser(){
+  async getUser() {
     //const id = +this.route.snapshot.paramMap.get('id');
     this.user = this.authenticationService.getUser();
     console.log(this.user);
   }
 
-  handleFileInput(file: FileList){
-    this.fileToUpload= file.item(0);
+  handleFileInput(file: FileList) {
+    this.fileToUpload = file.item(0);
 
     //Show image preview
-    var reader= new FileReader();
+    var reader = new FileReader();
     reader.readAsDataURL(this.fileToUpload);
     let text = reader;
-    reader.onload = (event:any) =>{
-      console.log(text.result)
+    reader.onload = (event: any) => {
+      console.log(text.result);
       this.imageUrl = <string>text.result;
       this.picture = <string>text.result;
     };
   }
 
-  safePassword(){
-
+  async safePassword() {
+    await this.userManagement.postchangePassword(
+      this.password,
+      this.user.getId()
+    );
   }
-
 
   setValue() {
     this.firstname = this.user.getFirstname();
@@ -82,9 +83,12 @@ export class UserManagementComponent implements OnInit {
     console.log(this.user);
   }
 
-  async safe(){
-    if(this.password == this.passwordConfirm && this.email == this.emailConfirm){
-      console.log(this.picture);
+  async safe() {
+    if (
+      this.password == this.passwordConfirm &&
+      this.email == this.emailConfirm
+    ) {
+      console.log(this.user);
       await this.userManagement.changeUserData(
         this.user.getId(),
         this.username,
@@ -98,14 +102,36 @@ export class UserManagementComponent implements OnInit {
         this.birthday,
         this.email,
         this.picture
+      ).then((data) =>{
+        this.setUser()
+      });
+
+        console.log(this.user);
+        window.alert('Ihre Daten wurden erfolgreich geändert.');
+      } else {
+        window.alert(
+          'Bitte alle Felder ausfüllen oder die Email / das Passwort überprüfen.'
         );
-      //await this.userManagement.postchangePassword( this.password, this.user.getId());
-      //@Frontend, wenn erfolreich dann set-Methode um die lokalen Werte des Users zu überschreiben
-      window.alert('Ihre Daten wurden erfolgreich geändert.');
+      }
     }
-    else{
-      window.alert('Bitte alle Felder ausfüllen oder die Email / das Passwort überprüfen.');
+  
+
+    setUser(){
+       //@Frontend, wenn erfolreich dann set-Methode um die lokalen Werte des Users zu überschreiben
+       if (this.userManagement.getErrorMessage() == undefined) {
+        this.user = this.authenticationService.changeUserdata(
+          this.username,
+          this.firstname,
+          this.name,
+          this.gender,
+          this.street,
+          this.houseNumber,
+          this.postalCode,
+          this.city,
+          this.birthday,
+          this.email,
+          this.picture
+        );
     }
   }
-
 }
